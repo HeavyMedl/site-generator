a16f734fdc338fef05015fe7801ede4cd945aa07
kurtlocker <kurtdmedley@gmail.com>
2016-03-13 16:27:47 -0700

    messing with constants

--- scripts/transform.js
+++ scripts/transform.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
-const $ = require('./lib/log.js');
-const args = process.argv.slice(2);
-const config = require(args.length > 0 
-	? args[0] : '../config.json');
+const $ 		= require('./lib/log.js'),
+const args 		= process.argv.slice(2),
+const config 	= require(args.length > 0 ? args[0] 
+					: '../config.json');
 
-console.log(config.description);
+console.log(config.body.description);
