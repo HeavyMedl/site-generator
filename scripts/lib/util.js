094fc97460ee441b400d64eb6cd36fbf241fcf16
kurtlocker <kurtdmedley@gmail.com>
2016-03-14 16:23:50 -0700

    renamed log to util cause i want to put other util methods in there.

--- scripts/lib/util.js
+++ scripts/lib/util.js
@@ -26,5 +26,28 @@ module.exports = {
 			yllw      : '33'
 		}
 		return '\x1b['+codes[code]+'m';
+	},
+	/**
+	 * [description]
+	 * @param  {[type]} path [description]
+	 * @return {[type]}      [description]
+	 */
+	mkdirSync : (path) => {
+		safe(fs.mkdirSync)(path);
+	},
+	/**
+	 * [description]
+	 * @param  {Function} fn [description]
+	 * @return {[type]}      [description]
+	 */
+	safe : (fn) => {
+		return () => {
+			try {
+				return fn.apply(this, arguments);
+			} catch(ex) {
+				console.trace(ex.stack);
+				process.exit(1);
+			}
+		}
 	}
 }
