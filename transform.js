4dc9d8afb1abe54373286d48f8282fcec0b6c25b
kurtlocker <kurtdmedley@gmail.com>
2016-03-15 13:13:58 -0700

    building relative paths for images

--- transform.js
+++ transform.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 const script = "transform.js";
-const path 	 = require('path');
-const fs 	 = require('fs');
-const ncp 	 = require('ncp');
-const chalk  = require('chalk');
-const args 	 = process.argv.slice(2);
-const config = require(args.length > 0 ? args[0] : './config.json');
-const source = path.normalize(config.theme);
-const dest 	 = path.normalize(`${config.out}/${config.body.title}`);
+const path 		= require('path');
+const fs 		= require('fs');
+const wrench 	= require('wrench');
+const chalk  	= require('chalk');
+const args 		= process.argv.slice(2);
+const config 	= require(args.length > 0 ? args[0] : './config.json');
+const norm 		= path.normalize
+const source 	= norm(config.theme);
+const dest 		= norm(`${config.out}/${config.body.title}`);
 
 // terminal color methods
 const _script = chalk.cyan.bold.underline;
 const method = chalk.cyan;
 const desc = chalk.yellow;
+const succ = chalk.green;
 
 function safe(fn) {
 	return function() {
@@ -27,8 +29,42 @@ function safe(fn) {
 }
 
 function transform_data() {
-	// log this
-	let returnStr = "var data = ";
+	const name = ' transform_data ';
+	console.log(_script(script)+method(name)+desc(
+			'Configuring data.js object'));
+
+	let returnStr = "var data = ",
+		carousel = wrench.readdirSyncRecursive(
+			norm(config.header.carousel)),
+		all = wrench.readdirSyncRecursive(
+			norm(config.body.imgs));
+	
+	get_relative_paths(carousel, config.header.carousel);
+	get_relative_paths(all, config.body.imgs);
+
+	console.log(_script(script)+method(name)+succ(
+		'Done configuring data.js object'));
+}
+
+function get_relative_paths(img_arr, path_config) {
+	const name = ' get_relative_paths ';
+	console.log(_script(script)+method(name)+desc(
+			'Building list of relative paths'));
+
+	let imgs = [];
+	img_arr.forEach(img => {
+		let info = fs.statSync(norm(`${path_config}/${img}`));
+		if (info.isFile() && (path.extname(img) == '.jpg'||
+			path.extname(img)== '.png')) {
+			imgs.push(
+				norm(`/assets/img/imgs/${img.replace(/\s/g, "%20")}`)
+			);
+		}
+	});
+
+	console.log(_script(script)+method(name)+succ(
+		'Done building list of relative paths'));
+	return imgs;
 }
 
 function write_data_js(data) {
@@ -45,16 +81,14 @@ function write_data_js(data) {
 
 function copy_configured_theme() {
 	const name = ' copy_configured_theme ';
-	console.log(
-		_script(script)+method(name)+desc(`Copying from ${source} to ${dest}`));
+	console.log(_script(script)+method(name)+desc(
+			`Copying from ${source} to ${dest}`));
 
-	ncp(source, dest, err => {
-		if (err) {
-			console.trace(err.stack);
-			process.exit(1);
-		}
-		console.log(_script(script)+method(name)+desc('Done copying.'));
-	});
+	wrench.copyDirSyncRecursive(source, dest);
+	
+	console.log(_script(script)+method(name)+succ(
+		'Done copying.'));
 }
 
 safe(copy_configured_theme)();
+safe(transform_data)();
